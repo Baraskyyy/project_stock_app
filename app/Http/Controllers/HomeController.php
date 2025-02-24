13f57c812428ef9e1bb729a394821f061ad4cdc5
bismatsaniyah <bismatsaniyah@gmail.com>
2025-02-24 02:41:43 +0000

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BarangKeluar;
 use App\Models\pelanggan;
+use App\Models\stok;
 use App\Models\suplier;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,18 +13,28 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard');
+        $getSuplier = suplier::count();
+        $getPelanggan =pelanggan::count();
+        $getStok = stok::count();
+        $getPendapatan = BarangKeluar::sum('sub_total');
+
+        return view('dashboard.dashboard', compact(
+            'getSuplier',
+            'getStok',
+            'getPelanggan',            'getStok',
+            'getPendapatan'
+        ));
     }
 
-    public function hitung()
-{
-    $totalSuplier = suplier::count(); // Menghitung jumlah semua suplier
-    return view('dashboard.dashboard', compact('totalSuplier'));
+//     public function hitung()
+// {
+//     $totalSuplier = suplier::count(); // Menghitung jumlah semua suplier
+//     return view('dashboard.dashboard', compact('totalSuplier'));
 
-    $totalUsers = pelanggan::count(); // Menghitung jumlah semua user
-    return view('dashboard.dashboard', compact('totalUsers'));
+//     $totalUsers = pelanggan::count(); // Menghitung jumlah semua user
+//     return view('dashboard.dashboard', compact('totalUsers'));
 
 
-}
+// }
 
 }
